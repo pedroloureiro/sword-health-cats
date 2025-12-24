607c35899aadcc1070c3118023282cfead025a24
Pedro Loureiro <pedro.loureiro@luggit.app>
2025-12-24 01:55:03 +0000

    feat: implement calls to get, set and delete favourites in CatsProcess

--- app/src/main/java/com/sword/cats/domain/cats/CatsProcess.kt
+++ app/src/main/java/com/sword/cats/domain/cats/CatsProcess.kt
@@ -2,6 +2,9 @@ package com.sword.cats.domain.cats
 
 import com.sword.cats.data.api.cats.CatsService
 import com.sword.cats.data.api.models.CatDto
+import com.sword.cats.data.api.models.CatFavouriteDto
+import com.sword.cats.data.api.models.FavouriteApiRequest
+import com.sword.cats.data.api.models.FavouriteApiResponse
 import com.sword.cats.data.database.CatDao
 import com.sword.cats.data.database.CatEntity
 import com.sword.cats.extensions.mapToResult
@@ -10,6 +13,9 @@ interface CatsProcess {
     suspend fun search(): Result<List<CatDto>>
     suspend fun save(cats: List<CatEntity>)
     suspend fun searchDb(): Result<List<CatEntity>>
+    suspend fun getFavourites(): Result<List<CatFavouriteDto>>
+    suspend fun setFavourite(request: FavouriteApiRequest): Result<FavouriteApiResponse>
+    suspend fun deleteFavourite(favouriteId: String): Result<Unit>
 }
 
 class CatsProcessImpl(
@@ -26,5 +32,17 @@ class CatsProcessImpl(
 
     override suspend fun searchDb(): Result<List<CatEntity>> {
         return catDao.getAllCats().mapToResult()
+    }
+
+    override suspend fun getFavourites(): Result<List<CatFavouriteDto>> {
+        return service.getFavourites().mapToResult()
+    }
+
+    override suspend fun setFavourite(request: FavouriteApiRequest): Result<FavouriteApiResponse> {
+        return service.setFavourite(request).mapToResult()
+    }
+
+    override suspend fun deleteFavourite(favouriteId: String): Result<Unit> {
+        return service.deleteFavourite(favouriteId).mapToResult()
     }
 }
