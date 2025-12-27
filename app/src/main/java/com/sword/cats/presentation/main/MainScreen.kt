f540aeabae0293e7c5351eb2addd2bfbc2bf11b2
Pedro Loureiro <pedro.loureiro@luggit.app>
2025-12-27 15:44:37 +0000

    fix: CatScreenDetails appears with no bottom bar

--- app/src/main/java/com/sword/cats/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sword/cats/presentation/main/MainScreen.kt
@@ -4,18 +4,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.sword.cats.presentation.components.BottomBar
 import com.sword.cats.presentation.navigation.AppNavHost
+import com.sword.cats.presentation.navigation.tabRowScreens
 
 @Composable
 fun MainScreen() {
+    val navController = rememberNavController()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val showBottomBar = currentRoute in tabRowScreens.map { it.route }
+
     MaterialTheme {
-        val navController = rememberNavController()
         Scaffold(
             bottomBar = {
-                BottomBar(navController)
+                if (showBottomBar) {
+                    BottomBar(navController)
+                }
             }
         ) { innerPadding ->
             AppNavHost(
